dca0caf7aaabab5b3a6270a687dce8a08c151375
Krzysztof Swędzioł <127301293+KrzysztofSwedziol@users.noreply.github.com>
2023-04-21 19:02:20 +0200

    Add files via upload

---
+++ algorytmy grafowe/DFS on List of Lists.py
@@ -0,0 +1,46 @@
+# A-----------C\
+# |           |  \
+# |           |    \
+# |           |     E
+# |           |    /
+# |           |  /
+# B-----------D/
+
+#-------------------------List Graph with parameters without dictionaries-----------------------------------------------
+
+#A--->0, B--->1, C--->2, D--->3, E--->4
+
+graph = [[1,2], [0, 3], [0, 3, 4], [1, 2, 4], [2, 3]]
+visited = [False for i in range(5)]
+parents = [None for j in range(5)]
+
+for x in range(5):
+    print(x, '--->', graph[x])
+
+time = -1
+
+
+def explore(graph, node):
+    print("current node : ", node)
+    print("current node's parent : ", parents[node])
+    global time
+    time +=1
+    print(time)
+    visited[node] = True
+    for neighbour in graph[node]:
+        if visited[neighbour] == False:
+            parents[neighbour] = node
+            explore(graph, neighbour)
+
+def DFS(graph, visited, parents):
+    for v in range(len(graph)):
+        visited[v] = False
+        parents[v] = None
+    global time
+    time = -1
+    for v in range(len(graph)):
+        if visited[v] == False :
+            explore(graph, v)
+
+
+DFS(graph, visited, parents)
