e958a1e55ea1bf8bf702a7a26b06c7edffbb958c
Krzysztof Swędzioł <127301293+KrzysztofSwedziol@users.noreply.github.com>
2023-04-27 23:27:18 +0200

    Add files via upload

---
+++ algorytmy grafowe/Dijkstra Algorithm/Dijkstra Algorithm.py
@@ -0,0 +1,34 @@
+from math import inf
+from queue import PriorityQueue
+
+graph = [[(1, 1), (7, 2)], [(0, 1), (2, 3), (4, 3)], [(1, 3), (3, 5)], [(2, 5), (4, 2), (6, 1)], [(1, 3), (3, 2), (5, 3), (7, 1)], [(4, 3), (6, 8), (8,1)], [(5,8), (3, 1), (8, 4)], [(0, 2), (4, 1), (8, 7)], [(7, 7), (5, 1), (6, 4)] ]
+
+
+def relax(G, s, node, neighbour_num, distance, parents, node_to_neigh, PriorQ ):
+    if distance[neighbour_num] > distance[node] + node_to_neigh:
+        distance[neighbour_num] = distance[node] + node_to_neigh
+        parents[neighbour_num] = node
+        PriorQ.put((distance[neighbour_num], neighbour_num))
+
+
+
+def Dijkstra(G, s):
+    parents = [None for i in range(len(G))]
+    distance = [inf for j in range(len(G))]
+    distance[s] = 0
+    PriorQ = PriorityQueue()
+    PriorQ.put((0, s))
+    while PriorQ.qsize() > 0:
+        curr = PriorQ.get()
+        node = curr[1]
+        for neighbour in (graph[node]):
+            node_to_neigh = neighbour[1]
+            neighbour_num = neighbour[0]
+            relax(G, s, node, neighbour_num, distance,parents,  node_to_neigh, PriorQ)
+    return distance
+
+
+a = Dijkstra(graph, 0)
+print(a)
+
+
