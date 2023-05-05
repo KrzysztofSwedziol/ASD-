4ba007e3492226b3837b650ff1583b4e5258c8ee
Krzysztof Swędzioł <127301293+KrzysztofSwedziol@users.noreply.github.com>
2023-05-05 18:10:21 +0200

    Add files via upload

---
+++ algorytmy grafowe/Floyd-Warshall Algorithm.py
@@ -0,0 +1,41 @@
+from math import inf
+
+graph = [
+    [0, 3, 8, inf, -4],
+    [inf, 0, inf, 1, 7],
+    [inf, 4, 0, inf, inf],
+    [2, inf, -5, 0, inf],
+    [inf, inf, inf, 6, 0]
+]
+
+
+def Floyd_Warshall(G):
+    n = len(G)
+    distance = [[inf for i in range(n)] for j in range(n)]
+    parent = [[None for i in range(n)] for j in range(n)]
+
+    for p in range(n):
+        for u in range(n):
+            if G[p][u] == 0:
+                distance[p][u] = inf
+            else:
+                distance[p][u] = G[p][u]
+        distance[p][p] = 0
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+
+
+    for i in range(n):
+
+        if distance[i][i] < 0:
+            return False
+
+    for i in range(n):
+        print(distance[i])
+
+    return distance, parent
+
+Floyd_Warshall(graph)
