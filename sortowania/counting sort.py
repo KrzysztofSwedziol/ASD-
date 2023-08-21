8247ca9db5b4f4a49f05453a73c589c447701111
Krzysztof Swędzioł <127301293+KrzysztofSwedziol@users.noreply.github.com>
2023-08-21 16:58:25 +0200

    Update counting sort.py

--- sortowania/counting sort.py
+++ sortowania/counting sort.py
@@ -2,10 +2,11 @@ tab = [3,6,8,2,1,8,6,15,10,11,7,3]
 #[1,2,3,3,6,6,7,8,8,10,11,15]
 print(tab)
 print("zakres 1-15")
-tab2=[0 for i in range(0, 16)]
 
 
 def Counting_Sort(tab):
+    maximum = max(tab)
+    tab2 = [0 for i in range(maximum + 1)]
     for i in range(len(tab)):
         tab2[tab[i]]+=1
     j=0
@@ -14,7 +15,6 @@ def Counting_Sort(tab):
             tab[j]=m
             j+=1
             tab2[m]=tab2[m]-1
-
 
 Counting_Sort(tab)
 print(tab)
